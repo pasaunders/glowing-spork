b2f7d958c286f8ea6eacb2639d4b25a8afb5b9b8
Patrick <kaorti@gmail.com>
2017-12-04 16:31:31 -0800

    why is this working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,16 @@ import { DataService } from './data.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
-  count : number = 0;
+  count : string;
   showGold () {
     return this._dataService.count;
   }
   constructor(private _dataService : DataService) { }
+
+  ngOnInit() {
+    this.count = this._dataService.count.toString();
+    console.log(this.count);
+  }
 }
